5e5d14563f7d596d0c78e53b2767197b16341f88
GrossTrevor <grosstrevor000@gmail.com>
2023-11-04 22:07:45 -0400

    page rank

--- AdjacencyList.h
+++ AdjacencyList.h
@@ -1,14 +1,19 @@
 #pragma once
+#include <map>
 
 class AdjacencyList {
-	map<string, vector<string>> graph;
+	map<string, vector<string>> adjTo;
+	map<string, vector<string>> adjFrom;
 
 public:
 	AdjacencyList();
 	~AdjacencyList();
-	void addVertex(string vertex);
-	void addEdge(string from, string to);
-	bool inGraph(string vertex);
+	void addVertexFrom(string vertex);
+	void addVertexTo(string vertex);
+	bool inGraphFrom(string vertex);
+	bool inGraphTo(string vertex);
+	void addFrom(string from, string to);
+	void addTo(string from, string to);
 	void pageRank(int powers);
 	void print();
 };
